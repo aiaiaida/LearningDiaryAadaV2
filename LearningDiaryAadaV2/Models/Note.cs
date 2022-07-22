790e975cd1828e0ff1a7071041e4d7ec6b5e119c
Aada Liu <aadaappelsiini@gmail.com>
2022-07-22 16:26:13 +0300

    CRUD views initial design

--- LearningDiaryAadaV2/Models/Note.cs
+++ LearningDiaryAadaV2/Models/Note.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,8 +11,14 @@ namespace LearningDiaryAadaV2.Models
     public partial class Note
     {
         public int Id { get; set; }
+        [Display(Name = "Topic ID")]
+        [DisplayFormat(NullDisplayText = "Not set")]
         public int? TopicId { get; set; }
+        [Display(Name = "Task ID")]
+        [DisplayFormat(NullDisplayText = "Not set")]
         public int? TaskId { get; set; }
+        [Display(Name = "Note")]
+        [DisplayFormat(NullDisplayText = "Not set")]
         public string Note1 { get; set; }
 
         public virtual TaskInTopic Task { get; set; }
--- LearningDiaryAadaV2/Models/Topic.cs
+++ LearningDiaryAadaV2/Models/Topic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,13 +18,29 @@ namespace LearningDiaryAadaV2.Models
         }
 
         public int Id { get; set; }
+        [Display(Name = "Title")]
+        [DisplayFormat(NullDisplayText = "Null")]
         public string Title { get; set; }
+        [Display(Name = "Description")]
+        [DisplayFormat(NullDisplayText = "Not set")]
         public string Description { get; set; }
+        [Display(Name = "Estimated time to master")]
+        [DisplayFormat(NullDisplayText = "Not set")]
         public int? EstimatedTimeToMaster { get; set; }
+        [Display(Name = "Time spent")]
+        [DisplayFormat(NullDisplayText = "Not set")]
         public int? TimeSpent { get; set; }
+        [Display(Name = "Source")]
+        [DisplayFormat(NullDisplayText = "Not set")]
         public string Source { get; set; }
+        [Display(Name = "Start learning date")]
+        [DisplayFormat(NullDisplayText = "Not set")]
         public DateTime? StartLearningDate { get; set; }
+        [Display(Name = "In progress")]
+        [DisplayFormat(NullDisplayText = "Not set")]
         public bool? InProgress { get; set; }
+        [Display(Name = "Completion date")]
+        [DisplayFormat(NullDisplayText = "Not set")]
         public DateTime? CompletionDate { get; set; }
 
         public virtual ICollection<Note> Notes { get; set; }
